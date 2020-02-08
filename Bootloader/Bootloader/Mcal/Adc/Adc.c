71eb9bbac4205d3e4abb8cceb571c6cce0f8b7b1
ShroukRashwan <shroukrashwan@gmail.com>
2020-02-08 23:51:27 +0200

    Added Adc functions prototype

--- Bootloader/Bootloader/Mcal/Adc/Adc.c
+++ Bootloader/Bootloader/Mcal/Adc/Adc.c
@@ -4,3 +4,6 @@
  * Created: 2/7/2020 8:21:58 PM
  *  Author: MahmoudMorsy
  */ 
+
+#include <avr/interrupt.h>
+#include "Adc.h"
--- Bootloader/Bootloader/Mcal/Adc/Adc.h
+++ Bootloader/Bootloader/Mcal/Adc/Adc.h
@@ -9,8 +9,11 @@
 #ifndef ADC_H_
 #define ADC_H_
 
+#include "Common/Common_Macros.h"
+#include "Common/Common_Types.h"
 
-
+void Adc_Init(uint8 channel);
+uint16 Adc_Read();
 
 
 #endif /* ADC_H_ */
