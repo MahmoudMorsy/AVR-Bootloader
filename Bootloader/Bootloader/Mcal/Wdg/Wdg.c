b2391160e513ab5fc813555bd267dfc4080d7019
Mahmoud Morsy <mahmoud_morsy@live.com>
2020-02-21 02:46:39 +0200

    Added the header comments and divided the code sections

--- Bootloader/Bootloader/Mcal/Wdg/Wdg.c
+++ Bootloader/Bootloader/Mcal/Wdg/Wdg.c
@@ -1,37 +1,30 @@
-/*
- * Wdg.c
- *
- * Created: 2/7/2020 8:23:43 PM
- *  Author: MahmoudMorsy
- */ 
+/**************************************************************************************************
+* FILE INFORMATION
+* -------------------------------------------------------------------------------------------------
+* NAME:              Wdg.c
+* MODULE:            Wdg
+* AUTHOR:            MahmoudMorsy
+* DESCRIPTION:       Watch dog timer driver functional implementation
+* CREATION DATE:     20/2/2020
+* MODIFICATION DATE: 21/2/2020
+**************************************************************************************************/
 #include "Wdg.h"
 #include <avr/io.h>
 
+/**************************************************************************************************
+*                                         GLOBAL VARIABLES                                        *
+**************************************************************************************************/
 /* Global variable to store the current configured pre-scaler */
-STATIC Wdg_OscCycles_en Wdg_CurrentPrescaler = OSC_16;
+STATIC Wdg_OscCycles_en Wdg_CurrentPrescaler                                              = OSC_16;
 /* Global variable to store the current watch dog status */
-STATIC Wdg_State_en Wdg_CurrentState = DISABLED;
+STATIC Wdg_State_en Wdg_CurrentState                                                    = DISABLED;
 
-void Wdg_Disable()
+/**************************************************************************************************
+*                                     FUNCTIONS IMPLEMENTATION                                    *
+**************************************************************************************************/
+void Wdg_AdjustPrescaler(Wdg_OscCycles_en OscCycles)
 {
-    /* Start Critical Section */
-    Sys_StartCriticalSection();
-    if (Wdg_CurrentState == RUNNING)
-    {
-        /* Reset Watch dog */
-        Wdg_Reset();
-        /* To disable WDG, Write 1 in WDTOE and WDE at the same time, then Write 0 in WDE within 4 clock cycles */
-        WDTCR |= (1<<WDTOE) | (1<<WDE);
-        WDTCR = 0;
-        /* Set driver's current state as disabled */
-        Wdg_CurrentState = DISABLED;        
-    }
-    else
-    {
-        /* Do Nothing */
-    }
-    /* Stop Critical Section */
-    Sys_StopCriticalSection();
+    Wdg_CurrentPrescaler = OscCycles;
 }
 
 void Wdg_Enable()
@@ -55,11 +48,6 @@ void Wdg_Enable()
     Sys_StopCriticalSection();
 }
 
-void Wdg_AdjustPrescaler(Wdg_OscCycles_en OscCycles)
-{
-    Wdg_CurrentPrescaler = OscCycles;
-}
-
 void Wdg_Feed()
 {
     if (Wdg_CurrentState == RUNNING)
@@ -71,3 +59,26 @@ void Wdg_Feed()
         /* Do Nothing */
     }
 }
+
+void Wdg_Disable()
+{
+    /* Start Critical Section */
+    Sys_StartCriticalSection();
+    if (Wdg_CurrentState == RUNNING)
+    {
+        /* Reset Watch dog */
+        Wdg_Reset();
+        /* To disable WDG, Write 1 in WDTOE and WDE at the same time, then Write 0 in WDE within 4 clock cycles */
+        WDTCR |= (1<<WDTOE) | (1<<WDE);
+        WDTCR = 0;
+        /* Set driver's current state as disabled */
+        Wdg_CurrentState = DISABLED;        
+    }
+    else
+    {
+        /* Do Nothing */
+    }
+    /* Stop Critical Section */
+    Sys_StopCriticalSection();
+}
+
